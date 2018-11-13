cf4527e17622fc7e87d0819386f33dc4892e2f40
Annab96 <lannab082@gmail.com>
2018-11-13 14:16:58 +0900

    자바프젝 14장_1

---
+++ JAVA/18_Java Project/src/jp14/Example7.java
@@ -0,0 +1,11 @@
+package jp14;
+
+import java.util.Arrays;
+
+public class Example7 {
+	public static void main(String[] args) {
+		String s = "one two, three,\t four; \t\nfive..,six";
+		String[] a = s.split("[ ,.;\t\n]+");
+		System.out.println(Arrays.toString(a));
+	}
+}
