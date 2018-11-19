bf2b069256650a29e5196b162332bd51c337712b
Annab96 <lannab082@gmail.com>
2018-11-19 14:32:55 +0900

    자바프젝 15장

---
+++ JAVA/18_Java Project/src/jp15/Example4.java
@@ -0,0 +1,26 @@
+package jp15;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+public class Example4 {
+	
+	public static void main(String[] args) {
+		Collection<Integer> c = new ArrayList<Integer>();
+
+		for (int i = 0; i < 10; ++i)
+			c.add(i);
+		
+		for (int i : c)
+			System.out.print(i + " ");
+	
+		System.out.println();
+		
+		for (int i : c)
+			if (i % 2 == 0)
+				c.remove(i);
+		
+		for (int i : c)
+			System.out.print(i + " ");
+	}
+}
