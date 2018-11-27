9210acb20ee8234e8b5f9d1c6eb45d7dfbf77b4a
Annab96 <lannab082@gmail.com>
2018-11-27 22:08:57 +0900

    1126 실습문제_1

---
+++ JAVA/H201735026/exercise/Example2.java
@@ -0,0 +1,28 @@
+package exercise;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+
+public class Example2 {
+
+	static void solution(List<Integer> a) {
+		// 목록 a에서 2의 배수와 3의 배수를 모두 제거하라
+		Iterator<Integer> it = a.iterator();
+		while (it.hasNext()) {
+			int number = it.next();
+			if (number % 2 == 0 || number % 3 == 0)
+				it.remove();
+		}
+
+	}
+
+	public static void main(String[] args) {
+		List<Integer> a = new ArrayList<>();
+		a.addAll(Arrays.asList(new Integer[] { 1, 6, 1, 8, 9, 2, 2, 3, 5, 3, 1, 3, 7 }));
+		System.out.println(a.toString());
+		solution(a);
+		System.out.println(a.toString());
+	}
+}
