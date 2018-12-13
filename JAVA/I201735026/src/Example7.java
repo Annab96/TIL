06643e718a3ab50a66183376511ced2be7eff6e6
Annab96 <lannab082@gmail.com>
2018-12-13 22:50:10 +0900

    1203 6-9

--- JAVA/I201735026/src/Example7.java
+++ JAVA/I201735026/src/Example7.java
@@ -4,21 +4,23 @@ import java.util.List;
 import java.util.Random;
 
 public class Example7 {
-	static void solution(List<Integer> a) {
-		for (int i = 0; i < a.size(); ++i)
-			if ((a.get(i) % 2 == 0) || (a.get(i) % 3 == 0)) {
-				a.remove(i);
-				i--;
-			}
-	}
 
-	public static void main(String[] args) {
-		Random random = new Random();
-		List<Integer> a = new ArrayList<>();
-		for (int i = 0; i < 30; ++i)
-			a.add(random.nextInt(8) + 1);
-		System.out.println(a.toString());
-		solution(a);
-		System.out.println(a.toString());
-	}
+    static void solution(List<Integer> a) {
+        Iterator<Integer> it = a.iterator();
+        while (it.hasNext()) {
+            int value = it.next();
+            if ((value % 2) == 0 || (value % 3) == 0)
+                it.remove();
+        }
+    }
+
+    public static void main(String[] args) {
+        Random random = new Random();
+        List<Integer> a = new ArrayList<>();
+        for (int i = 0; i < 30; ++i)
+            a.add(random.nextInt(8) + 1);
+        System.out.println(a.toString());
+        solution(a);
+        System.out.println(a.toString());
+    }
 }
--- JAVA/I201735026/src/Example9.java
+++ JAVA/I201735026/src/Example9.java
@@ -1,25 +1,20 @@
 import java.util.Date;
 
 public class Example9 {
-	static boolean sameClass(Object o1, Object o2) {
-		if(o1.getClass()==o2.getClass())
-			return true;
-		else
-			return false;
-	}
 
-	static boolean sameClass(Object o1, Class cls) {
-		if(o1.getClass() == cls)
-			return true;
-		else
-			return false;
-	}
+    static boolean sameClass(Object o1, Object o2) {
+        return o1.getClass() == o2.getClass();
+    }
 
-	public static void main(String[] args) {
-		String s1 = "hello", s2 = "world";
-		Date d = new Date();
-		System.out.println(sameClass(s1, d));
-		System.out.println(sameClass(s1, s2));
-		System.out.println(sameClass(s1, String.class));
-	}
+    static boolean sameClass(Object o1, Class cls) {
+        return o1.getClass() == cls;
+    }
+
+    public static void main(String[] args) {
+        String s1 = "hello", s2 = "world";
+        Date d = new Date();
+        System.out.println(sameClass(s1, d));
+        System.out.println(sameClass(s1, s2));
+        System.out.println(sameClass(s1, String.class));
+    }
 }
